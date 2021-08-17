059e87561391d11c72688d1d3fe7b35d71902570
smithachawalmane <33485117+smithachawalmane@users.noreply.github.com>
2021-08-17 23:22:18 +0530

    Add files via upload

---
+++ BasketBallApplcation/BinaryTree.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasketBallApplcation
+{
+   public class BinaryTree
+    {
+		public Node root;
+
+		// Tree Node
+		public class Node
+		{
+			public int data;
+			public Node left, right;
+			public Node(int data)
+			{
+				this.data = data;
+				this.left = null;
+				this.right = null;
+			}
+		}
+
+		// Function to insert nodes in level order
+		public Node InsertLevelOrder(int[] arr,
+								Node root, int i)
+		{
+			// Base case for recursion
+			if (i < arr.Length)
+			{
+				Node temp = new Node(arr[i]);
+				root = temp;
+
+				// insert left child
+				root.left = InsertLevelOrder(arr,
+								root.left, 2 * i + 1);
+
+				// insert right child
+				root.right = InsertLevelOrder(arr,
+								root.right, 2 * i + 2);
+			}
+			return root;
+		}
+
+		public object SearchElement_Rec(object element, Node root)
+		{
+
+			Node current = root;
+
+			if (current == null)
+
+				return "Not found";
+
+			if (Convert.ToInt32(element) == Convert.ToInt32(current.data))
+			{
+				return current;
+			}
+
+			if (Convert.ToInt32(element) < Convert.ToInt32(current.data))
+
+				return this.SearchElement_Rec(element, current.left);
+
+			else
+
+				return this.SearchElement_Rec(element, current.right);
+
+		}
+
+		// Function to print tree
+		// nodes in InOrder fashion
+		public void InOrder(Node root)
+		{
+			if (root != null)
+			{
+				InOrder(root.left);
+				Console.WriteLine(root.data + " ");
+				InOrder(root.right);
+			}
+		}
+	}
+}
---
+++ BasketBallApplcation/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace BasketBallApplcation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree binaryTree = new BinaryTree();
+            int[] arr = { 26, 28, 31,40, 33, 36, 39 };
+            int[] player = new int[arr.Length + 1];
+            arr.CopyTo(player, 0);
+            binaryTree.root= binaryTree.InsertLevelOrder(arr, binaryTree.root, 0);
+            binaryTree.InOrder(binaryTree.root);
+            Array.Sort(player);
+            int[] sortedArray = player.Reverse().ToArray();
+            bool found = false;
+
+            for (int i = 0; i < sortedArray.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (j == 0 && sortedArray[i] == arr[j])
+                    {
+                        break;
+                    }
+                    else if (j % 2 != 0 && sortedArray[i] == arr[j])
+                    {
+                        break;
+                    }
+                    else if (j % 2 == 0 && sortedArray[i] == arr[j])
+                    {                       
+                        Console.WriteLine("player with the ball " + arr[j]+" is the winner. " );
+
+                        found = true;
+                        break;
+                    }
+                }
+                if (found)
+                {
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
